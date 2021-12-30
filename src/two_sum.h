87b66b2b28a6cfcc45ba9df58d2a0803362c04d5
lipingan.dev@outlook.com <lipingan@lipingandeMacBook-Pro.local>
2021-12-30 22:38:03 +0800

    [Solved] two sum.

---
+++ src/two_sum.h
@@ -0,0 +1,15 @@
+/**
+ * @file two_sum.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-12-30
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include <vector>
+namespace leetcode {
+std::vector<int> twoSum(std::vector<int>& nums, int target);
+}  // namespace leetcode
---
+++ test/two_sum_unittest.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file two_sum_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2021-12-30
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
+
+#include "two_sum.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <vector>
+TEST(two_sum, case_0) {
+  std::vector<int> nums{2, 7, 11, 15};
+  int target = 9;
+  auto result = leetcode::twoSum(nums, target);
+  std::vector<int> expect{0, 1};
+  EXPECT_EQ(result, expect);
+}
+
+TEST(two_sum, case_1) {
+  std::vector<int> nums{3, 2, 4};
+  int target = 6;
+  auto result = leetcode::twoSum(nums, target);
+  std::vector<int> expect{1, 2};
+  EXPECT_EQ(result, expect);
+}
+
+TEST(two_sum, case_2) {
+  std::vector<int> nums{3, 3};
+  int target = 6;
+  auto result = leetcode::twoSum(nums, target);
+  std::vector<int> expect{0, 1};
+  EXPECT_EQ(result, expect);
+}
