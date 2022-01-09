be711aec7d5f48962e05dd6cb4a970b7a1aa72f0
LiPingan <lipingan.dev@outlook.com>
2022-01-09 14:56:40 +0800

    [Solved] delete duplicates.

---
+++ src/delete_duplicates.h
@@ -0,0 +1,19 @@
+/**
+ * @file delete_duplicates.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-09
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __LEETCODE_DELETE_DUPLICATES_H__
+#define __LEETCODE_DELETE_DUPLICATES_H__
+#include "list_node.h"
+namespace leetcode {
+ListNode *deleteDuplicates(ListNode *head);
+} // namespace leetcode
+
+#endif
---
+++ test/delete_duplicates_unittest.cpp
@@ -0,0 +1,23 @@
+/**
+ * @file delete_duplicates_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-09
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "delete_duplicates.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(delete_duplicates, case_0) {
+  std::vector<int> vector{1, 1, 1, 1, 1, 1, 1, 1, 2, 3, 4, 4, 4, 4, 4, 5, 5,
+                          5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 9, 9};
+  leetcode::ListNode *head = leetcode::spawnList(vector);
+  leetcode::printList(head);
+  leetcode::deleteDuplicates(head);
+  leetcode::printList(head);
+}
