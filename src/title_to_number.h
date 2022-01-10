3ca4f52832fe942bce401d7d9a3915922e07d629
LiPingan <lipingan.dev@outlook.com>
2022-01-10 23:13:28 +0800

    [Solved] title to number.

---
+++ src/title_to_number.h
@@ -0,0 +1,19 @@
+/**
+ * @file title_to_number.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-10
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __LEETCODE_TITLE_TO_NUMBER_H__
+#define __LEETCODE_TITLE_TO_NUMBER_H__
+#include "string"
+namespace leetcode {
+int titleToNumber(std::string columnTitle);
+} // namespace leetcode
+
+#endif
---
+++ test/title_to_number_unittest.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file title_to_number_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-10
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "title_to_number.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(title_to_number, case_0) { std::cout << leetcode::titleToNumber("Z"); }
+
+TEST(title_to_number, case_1) { std::cout << leetcode::titleToNumber("FXSHRXW"); }
