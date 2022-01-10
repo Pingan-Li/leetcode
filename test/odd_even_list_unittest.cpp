b01127f1130a7635fdae725c096970dcf53f4e8c
LiPingan <lipingan.dev@outlook.com>
2022-01-10 22:37:32 +0800

    [Solved] odd even list.

---
+++ test/odd_even_list_unittest.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file odd_even_list_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-10
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "odd_even_list.h"
+#include "gtest/gtest.h"
+
+TEST(odd_even_list, case_0) {
+  leetcode::ListNode *head =
+      leetcode::spawnList(std::vector<int>{1,2,3});
+  leetcode::printList(head);
+  leetcode::ListNode *ret = leetcode::oddEvenList(head);
+  leetcode::printList(ret);
+}
