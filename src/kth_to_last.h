4b26039607694ff70d11a3353d1c781e0c3c0c88
LiPingan <lipingan.dev@outlook.com>
2022-01-11 23:49:12 +0800

    [Solved] kth to last.

---
+++ src/kth_to_last.h
@@ -0,0 +1,19 @@
+/**
+ * @file kth_to_last.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-11
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __LEETCODE_KTH_TO_LAST_H__
+#define __LEETCODE_KTH_TO_LAST_H__
+#include "list_node.h"
+namespace leetcode {
+int kthToLast(ListNode *head, int k);
+} // namespace leetcode
+
+#endif
---
+++ test/kth_to_last_unittest.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file kth_to_last_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-11
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "kth_to_last.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(kth_to_last, case_0) {
+  leetcode::ListNode *head = leetcode::spawnList(std::vector{1, 2, 3, 4, 5});
+  std::cout << leetcode::kthToLast(head, 1);
+}
