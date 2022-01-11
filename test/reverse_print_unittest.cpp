83369b2c48d2c6aca59e17b5c016b31709fc1846
LiPingan <lipingan.dev@outlook.com>
2022-01-11 23:00:20 +0800

    [Solved] other solution for reverse print.

---
+++ test/reverse_print_unittest.cpp
@@ -0,0 +1,31 @@
+/**
+ * @file reverse_print_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-11
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#include "reverse_print.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(reverse_print, case_0) {
+  std::vector<int> input{1, 2, 3, 4, 5};
+  leetcode::ListNode *head = leetcode::spawnList(input);
+  std::vector<int> ret = leetcode::reversePrint(head);
+  for (auto iter = ret.begin(); iter != ret.end(); ++iter) {
+    std::cout << *iter << ", ";
+  }
+}
+
+TEST(reverse_print, case_1) {
+  std::vector<int> input{1, 2, 3, 4, 5};
+  leetcode::ListNode *head = leetcode::spawnList(input);
+  std::vector<int> ret = leetcode::reversePrint2(head);
+  for (auto iter = ret.begin(); iter != ret.end(); ++iter) {
+    std::cout << *(iter) << ", ";
+  }
+}
