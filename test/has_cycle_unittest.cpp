f677cfaec4c373925b166bbf90a9a3d59c86b357
LiPingan <lipingan.dev@outlook.com>
2022-01-14 00:20:13 +0800

    [Solved] has cycle.

---
+++ test/has_cycle_unittest.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file has_cycle_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "has_cycle.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(has_cycle, case_0) {}
