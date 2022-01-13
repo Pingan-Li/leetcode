fb97dcf6a2aad0d95aa353aea909c087fe3cff19
LiPingan <lipingan.dev@outlook.com>
2022-01-13 21:58:28 +0800

    [Solved] is palindrome.

---
+++ test/is_palindrome_unittest.cpp
@@ -0,0 +1,42 @@
+/**
+ * @file is_palindrome_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-13
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "is_palindrome.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(is_palindrome, case_0) {
+  leetcode::ListNode *head =
+      leetcode::spawnList(std::vector<int>{1, 2, 3, 2, 1});
+  bool result = leetcode::isPalindrome(head);
+  EXPECT_TRUE(result);
+}
+
+TEST(is_palindrome, case_1) {
+  leetcode::ListNode *head =
+      leetcode::spawnList(std::vector<int>{1});
+  bool result = leetcode::isPalindrome(head);
+  EXPECT_TRUE(result);
+}
+
+TEST(is_palindrome, case_2) {
+  leetcode::ListNode *head =
+      leetcode::spawnList(std::vector<int>{});
+  bool result = leetcode::isPalindrome(head);
+  EXPECT_TRUE(result);
+}
+
+TEST(is_palindrome, case_3) {
+  leetcode::ListNode *head =
+      leetcode::spawnList(std::vector<int>{1,2,3});
+  bool result = leetcode::isPalindrome(head);
+  EXPECT_FALSE(result);
+}
