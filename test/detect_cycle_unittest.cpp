fc20425f4106a5a411fa10f7a38adc25e6d25fa0
LiPingan <lipingan.dev@outlook.com>
2022-01-14 00:52:56 +0800

    [Solved] detect cycle.

---
+++ test/detect_cycle_unittest.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file detect_cycle_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-14
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "detect_cycle.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+TEST(detect_cycle, case_0) {}
