405754d838072b7cfd657e147e53c5b6bae4eb65
LiPingan <lipingan.dev@outlook.com>
2022-01-19 00:00:28 +0800

    [Solved] get decimal value

---
+++ src/get_decimal_value.h
@@ -0,0 +1,20 @@
+/**
+ * @file get_decimal_value.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-18
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __LEETCODE_GET_DECIMAL_VALUE_H__
+#define __LEETCODE_GET_DECIMAL_VALUE_H__
+
+#include "list_node.h"
+namespace leetcode {
+int getDecimalValue(ListNode *head);
+} // namespace leetcode
+
+#endif
---
+++ test/get_decimal_value_unittest.cpp
@@ -0,0 +1,48 @@
+/**
+ * @file get_decimal_value_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-18
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "get_decimal_value.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace leetcode {
+TEST(get_decimal_value, case_0) {
+  std::vector<int> vec{1};
+  ListNode *head = spawnList(vec);
+  int expect = 1;
+  int result = getDecimalValue(head);
+  EXPECT_EQ(expect, result);
+}
+
+TEST(get_decimal_value, case_1) {
+  std::vector<int> vec{};
+  ListNode *head = spawnList(vec);
+  int expect = 0;
+  int result = getDecimalValue(head);
+  EXPECT_EQ(expect, result);
+}
+
+TEST(get_decimal_value, case_2) {
+  std::vector<int> vec{1,0,1,0,1};
+  ListNode *head = spawnList(vec);
+  int expect = 21;
+  int result = getDecimalValue(head);
+  EXPECT_EQ(expect, result);
+}
+
+TEST(get_decimal_value, case_3) {
+  std::vector<int> vec{1,0,0,1,0,0,1,1,1,0,0,0,0,0,0};
+  ListNode *head = spawnList(vec);
+  int expect = 18880;
+  int result = getDecimalValue(head);
+  EXPECT_EQ(expect, result);
+}
+} // namespace leetcode
