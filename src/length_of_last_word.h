52931d5006ea11bb2b82501e22e5c7ca3df0021c
LiPingan <lipingan.dev@outlook.com>
2022-01-22 23:02:41 +0800

    [Solved] length of last word.

---
+++ src/length_of_last_word.h
@@ -0,0 +1,18 @@
+/**
+ * @file length_of_last_word.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-22
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __LEETCODE_LENGTH_OF_LAST_WORD_H__
+#define __LEETCODE_LENGTH_OF_LAST_WORD_H__
+#include "string"
+namespace leetcode {
+int lengthOfLastWord(std::string s);
+} // namespace leetcode
+#endif
---
+++ test/length_of_last_word_unittest.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file length_of_last_word_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-22
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "length_of_last_word.h"
+
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace leetcode {
+TEST(length_of_last_world, case_0) {
+  int size = lengthOfLastWord("fly me to the moonn      ");
+  EXPECT_EQ(size, 5);
+}
+TEST(length_of_last_world, case_1) {
+  int size = lengthOfLastWord("a");
+  EXPECT_EQ(size, 1);
+}
+} // namespace leetcode
