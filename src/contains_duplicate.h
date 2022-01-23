e13c2a67c85c8a5ee6d7c0ca9baef1e95fb6fdf4
LiPingan <lipingan.dev@outlook.com>
2022-01-23 21:15:29 +0800

    [Solved] contains duplicate.

---
+++ src/contains_duplicate.h
@@ -0,0 +1,19 @@
+/**
+ * @file contains_duplicate.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-23
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __LEETCODE_CONTAINS_DUPLICATES_H__
+#define __LEETCODE_CONTAINS_DUPLICATES_H__
+#include "vector"
+namespace leetcode {
+bool containsDuplicate(std::vector<int> &nums);
+} // namespace leetcode
+
+#endif
---
+++ test/contains_duplicate_unittest.cpp
@@ -0,0 +1,40 @@
+/**
+ * @file contains_duplicate_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-23
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "contains_duplicate.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace leetcode {
+TEST(contains_duplicate, case_0) {
+  std::vector<int> vec{1, 1, 1, 2, 3, 4};
+  auto flag = containsDuplicate(vec);
+  EXPECT_TRUE(flag);
+}
+
+TEST(contains_duplicate, case_1) {
+  std::vector<int> vec{1,2,3};
+  auto flag = containsDuplicate(vec);
+  EXPECT_FALSE(flag);
+}
+
+TEST(contains_duplicate, case_2) {
+  std::vector<int> vec{1};
+  auto flag = containsDuplicate(vec);
+  EXPECT_FALSE(flag);
+}
+
+TEST(contains_duplicate, case_3) {
+  std::vector<int> vec;
+  auto flag = containsDuplicate(vec);
+  EXPECT_FALSE(flag);
+}
+} // namespace leetcode
