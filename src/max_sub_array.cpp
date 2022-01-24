b043eb37208b4b8c9c796bc8ab6774e7d4a286f5
LiPingan <lipingan.dev@outlook.com>
2022-01-24 21:31:58 +0800

    [Solved] max sub array.

---
+++ src/max_sub_array.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file max_sub_array.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-23
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "max_sub_array.h"
+#include "algorithm"
+namespace leetcode {
+/**
+ * @brief -
+ *
+ * @param nums
+ * @return int
+ */
+int maxSubArray(std::vector<int> &nums) {
+  int prev{0};
+  int result{nums.front()};
+  int temp{0};
+  for (auto iter = nums.begin(); iter != nums.end(); ++iter) {
+    temp = prev + *iter;
+    prev = temp > *iter ? temp : *iter;
+    result = result > prev ? result : prev;
+  }
+  return result;
+}
+} // namespace leetcode
---
+++ test/max_sub_array_unittest.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file max_sub_array_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-23
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+#include "max_sub_array.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+namespace leetcode {
+TEST(max_sub_array, case_0) {
+  std::vector<int> vector{-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  int result = maxSubArray(vector);
+  EXPECT_EQ(result, 6);
+}
+
+TEST(max_sub_array, case_1) {
+  std::vector<int> vector{1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int result = maxSubArray(vector);
+  EXPECT_EQ(result, 45);
+}
+
+TEST(max_sub_array, case_2) {
+  std::vector<int> vector{-1, 1, -1, 1, -1, 1};
+  int result = maxSubArray(vector);
+  EXPECT_EQ(result, 1);
+}
+} // namespace leetcode
