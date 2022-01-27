f868c494a9d23ac6519c94374281781f209fb19c
LiPingan <lipingan.dev@outlook.com>
2022-01-27 22:34:04 +0800

    [Solved] max profit.

---
+++ src/max_profit.h
@@ -0,0 +1,19 @@
+/**
+ * @file max_profit.h
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-27
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#ifndef __LEETCODE_MAX_PROFIX_H__
+#define __LEETCODE_MAX_PROFIX_H__
+#include "vector"
+namespace leetcode {
+int maxProfit(std::vector<int> &prices);
+} // namespace leetcode
+
+#endif
---
+++ test/max_profit_unittest.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file max_profit_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-27
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "max_profit.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+namespace leetcode {
+TEST(max_profit, case_0) {
+  std::vector<int> p{12, 2, 3, 4, 3, 3, 6, 9, 7, 34, 4, 2, 23};
+  int profit = maxProfit(p);
+  EXPECT_EQ(profit, 32);
+}
+} // namespace leetcode
