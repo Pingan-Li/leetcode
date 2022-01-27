977e6fd9118494d8a03811aac5c780ab576f084c
LiPingan <lipingan.dev@outlook.com>
2022-01-27 21:48:03 +0800

    [Solved] generate

---
+++ src/generate.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file generate.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-26
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "vector"
+namespace leetcode {
+std::vector<std::vector<int>> generate(int numRows) {
+  std::vector<std::vector<int>> ret(numRows);
+  for (size_t i = 0; i < numRows; ++i) {
+    ret[i].resize(i + 1);
+    ret[i][0] = 1;
+    ret[i][i] = 1;
+    for (int j = 1; j < i; ++j) {
+      ret[i][j] = ret[i - 1][j] + ret[i - 1][j - 1];
+    }
+  }
+  return ret;
+}
+} // namespace leetcode
---
+++ test/generate_unittest.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file generate_unittest.cpp
+ * @author lipingan (lipingan.dev@outlook.com)
+ * @brief
+ * @version 0.1
+ * @date 2022-01-27
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "generate.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+namespace leetcode {
+TEST(generate, case_0) {
+  std::vector<std::vector<int>> result = generate(25);
+  for (auto &vec : result) {
+    for (auto &i : vec) {
+      std::cout << i << " ";
+    }
+    std::cout << '\n';
+  }
+}
+} // namespace leetcode
